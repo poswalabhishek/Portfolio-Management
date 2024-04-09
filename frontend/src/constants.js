e075a41b15b4935e2c89b6796d0213493acbf3e3
Abhishek Poswal <poswalabhishek1234@icloud.com>
2024-04-09 19:52:02 +0800

    changing serverURL

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,5 @@
-export const serverURL = "http://localhost:8080/"
+export const serverURL = "http://127.0.0.1:8080/"
+// export const serverURL = "http://172.183.208.153/"
 export const colors = {
   positive: '#81C784',
   neutral: '#e0e0e0',
